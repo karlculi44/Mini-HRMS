5227defdccd17073e5ea7987ec53c5728682ea93
karlculi44 <kaimax187@gmail.com>
2026-06-20 17:27:19 +0800

    Update db.js
    Added a port for railway connection

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,6 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config();
-
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -11,11 +8,3 @@ const db = mysql.createPool({
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: 0,
 });
-
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-});
-
-export default db;
