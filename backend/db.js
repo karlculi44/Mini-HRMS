01119780f19e9132931bfa2875575c9313f17b8c
karlculi44 <kaimax187@gmail.com>
2026-06-20 17:35:43 +0800

    Update db.js
    fixed the db.js export

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,10 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT, // <-- add this
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -8,3 +12,5 @@ const db = mysql.createPool({
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: 0,
 });
+
+export default db;
